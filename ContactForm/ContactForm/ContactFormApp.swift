74f802ce7ff770a06d5970a93e728e855af2e5e0
BastianG09 <105691598+BastianG09@users.noreply.github.com>
2022-07-11 15:50:55 -0500

    Add files via upload

---
+++ ContactForm/ContactForm/ContactFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactFormApp.swift
+//  ContactForm
+//
+//  Created by BastianC on 6/24/22.
+//
+
+import SwiftUI
+
+@main
+struct ContactFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
